53673c1147f4601a5bd39bc63c54f7656485419a
Cody <74194707+lewisoutdoorllc@users.noreply.github.com>
2021-03-21 23:09:14 -0400

    made changes to routes and fixed bugs; more later

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
-const routes = require('./routes');
+const routes = require('./controllers');
 const sequelize = require('./config/connection');
-// const path = require('path');
-
+const path = require('path');
+// //IMPORT HANDLEBARS
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
 //IMPORT SESSIONS
 const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
@@ -19,9 +21,11 @@ const sess = {
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(session(sess));
 // turn on routes
 app.use(routes);
@@ -31,17 +35,15 @@ sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log(`Now listening on ${PORT}`));
 });
 
-// const express = require('express');
-// const routes = require('./controllers');
 
-// //IMPORT HANDLEBARS
-// const exphbs = require('express-handlebars');
+
+
+
 
 // //IMPORT HANDLEBARS HELPERS
 // const helpers = require('./utils/helpers');
-// const hbs = exphbs.create({ helpers });
 
-// app.engine('handlebars', hbs.engine);
-// app.set('view engine', 'handlebars');
+
+
 
 
