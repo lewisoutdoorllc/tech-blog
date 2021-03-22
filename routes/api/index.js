a09272f88243abef3d2c5c648cd27bfb61c97270
Cody <74194707+lewisoutdoorllc@users.noreply.github.com>
2021-03-21 21:49:57 -0400

    final changes to branch hopefully

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,12 +1,12 @@
 const router = require('express').Router();
 const userRoutes = require('./user-routes');
 const postRoutes = require('./post-routes');
-// const commentRoutes = require('./comment-routes');
+const commentRoutes = require('./comment-routes');
 
 router.use('/users', userRoutes);
 router.use('/posts', postRoutes);
 
-// router.use('/comments', commentRoutes);
+router.use('/comments', commentRoutes);
 
 // const router = require('express').Router();
 // const userRoutes = require('./user-routes.js');
