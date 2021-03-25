5c896fdcc3d2e2f223098ff2479e112bb6eff5a4
Cody <74194707+lewisoutdoorllc@users.noreply.github.com>
2021-03-25 16:20:33 -0400

    working on linking handlebars and JavaScript files; also working on functionality with the buttons and navbar

--- public/javascript/logout.js
+++ public/javascript/logout.js
@@ -6,7 +6,7 @@ async function logout() {
   
     if (response.ok) {
         console.log("You have been logged out");
-        document.location.replace('/');
+        document.location.replace('/login');
     } else {
         alert(response.statusText);
     }
