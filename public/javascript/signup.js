1f23eb35f9fac310ca8942e56967af21cf2da87c
Cody <74194707+lewisoutdoorllc@users.noreply.github.com>
2021-03-27 22:20:01 -0400

    Update signup.js

--- public/javascript/signup.js
+++ public/javascript/signup.js
@@ -6,7 +6,6 @@ async function signupFormHandler(event) {
   const password = document.querySelector("#password-signup").value.trim();
 
   if (username && email && password) {
-    // const response = await fetch("/api/users", {
     const response = await fetch("/api/users", {
       method: "post",
       body: JSON.stringify({
